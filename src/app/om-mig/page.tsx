e8a54ca028d8ae42baa63f27c2c1717f26bf6eb1
jacobcollen <jcbcollen@icloud.com>
2025-03-21 17:28:43 +0100

    about me page design

--- src/app/om-mig/page.tsx
+++ src/app/om-mig/page.tsx
@@ -6,8 +6,8 @@ import { images } from "@/app/_data/images";
 
 export default function AboutMe() {
   return (
-    <section className="container mx-auto max-w-6xl px-6 py-16">
-      <Card className="flex flex-col items-start gap-8 border-none bg-transparent p-8 shadow-none md:flex-row">
+    <section className="container mx-auto max-w-6xl py-16">
+      <Card className="flex flex-col gap-8 border-none bg-transparent shadow-none md:flex-row">
         <div className="w-full flex-shrink-0 md:w-1/3">
           <Image
             src={images.about}
@@ -18,7 +18,7 @@ export default function AboutMe() {
           />
         </div>
         <CardContent className="w-full md:w-2/3">
-          <CardHeader>
+          <CardHeader className="mb-4 p-0">
             <CardTitle className="text-3xl font-bold">Om mig</CardTitle>
           </CardHeader>
           <div className="mb-6 flex flex-wrap gap-2">
@@ -27,18 +27,18 @@ export default function AboutMe() {
             <Badge variant="outline">Forskare</Badge>
             <Badge variant="outline">Författare</Badge>
           </div>
-          <p className="mb-4 leading-relaxed">
+          <p className="text-md mb-4 leading-relaxed">
             Jag är socionom, lärare och forskare i socialt arbete på Örebro
             universitet. Innan jag kom till universitetsvärlden arbetade jag i
             över 20 år som kurator på Barn- och ungdomshabiliteringen samt inom
             elevhälsan.
           </p>
-          <p className="mb-4 leading-relaxed">
+          <p className="text-md mb-4 leading-relaxed">
             Jag föreläser främst för personal inom förskola, skola och
             vuxenutbildning kring teman som funktionsnedsättningar, psykisk
             hälsa och stöd i situationer av kris och trauma.
           </p>
-          <p className="mb-6 leading-relaxed">
+          <p className="text-md mb-6 leading-relaxed">
             År 2018 debuterade jag som författare med boken Alma och
             papegojmysteriet. Sedan dess har det blivit flera böcker för barn i
             olika åldrar samt en deckare i genren mysdeckare.
